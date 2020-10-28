c0d617a61a02122593de31f0b6053abec3163213
oceaster <oceaster@gmail.com>
2020-10-28 14:16:46 +0000

    fixed large division bug

--- solution.py
+++ solution.py
@@ -1,22 +1,12 @@
 
 
 def divide_sequence(seq, sets):
-    '''
-    :seq: takes list or set as declaration for original sequence
-    :sets:  takes integer as declaration for amount of new
-            equally sized arrays
-    :return: array of lists containing parts of original array
-            with total length declared by sets parameter
-    '''
-    size = len(seq) / sets
+    size = int(len(seq) / sets)
     curr = 0
     out = [list(), ]
 
-    if size > int(size):
-        size = int(size) + 1
-
     for value in seq:
-        if curr == size:
+        if curr >= size and len(out) < sets:
             curr = 0
             out.append(list())
         out[-1].append(value)
@@ -29,9 +19,7 @@ if __name__ == "__main__":
     '''
     tests that the declared function 'divide_sequence' works as
     intended when file is run as main and given value from terminal argument.
-
     Eg. python solution.py 5
-
     '''
     from sys import argv
 
---
+++ test.py
@@ -0,0 +1,8 @@
+from solution import divide_sequence
+
+array = [1,2,3]
+size = 2
+
+print(
+    divide_sequence(array, size)
+)
