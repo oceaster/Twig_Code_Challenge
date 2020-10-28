dbc14666189b5fca3e3c4689f810adf743d3631e
oceaster <oceaster@gmail.com>
2020-10-28 11:44:16 +0000

    fixed comment errors

--- solution.py
+++ solution.py
@@ -2,37 +2,36 @@
 
 def divide_sequence(seq, sets):
     '''
-    :seq: takes list or set as declaration for original sequence    
-    :sets:  takes integer as declaration for amount of new 
+    :seq: takes list or set as declaration for original sequence
+    :sets:  takes integer as declaration for amount of new
             equally sized arrays
-    :return: array of lists containing parts of original array 
-            with total length declared by sets parameter 
+    :return: array of lists containing parts of original array
+            with total length declared by sets parameter
     '''
     size = len(seq) / sets
     curr = 0
     out = [list(), ]
-    
-    if size > int(size):                # Adjust var for unequally sized divisons
+
+    if size > int(size):
         size = int(size) + 1
 
     for value in seq:
         if curr == size:
             curr = 0
             out.append(list())
-        if curr < size:
-            out[-1].append(value)
+        out[-1].append(value)
         curr += 1
 
     return out
 
 
 if __name__ == "__main__":
-    ''' 
-    tests that the declared function 'divide_array' works as
+    '''
+    tests that the declared function 'divide_sequence' works as
     intended when file is run as main and given value from terminal argument.
-    
+
     Eg. python solution.py 5
-    
+
     '''
     from sys import argv
 
